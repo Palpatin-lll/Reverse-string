4f24dff3f0a4760c109551f53ed554fd8e067d2a
Palpatin-lll <117033246+Palpatin-lll@users.noreply.github.com>
2022-11-15 13:17:32 +0300

    revstr

---
+++ reverse/reversestring/reversestring/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        /// <summary>
+        /// program start. The user enters a string, the program mirrors the word along the string
+        /// </summary>
+        static void Main(string[] args) /// <param> name="args">Command line arguments input.</param>
+        {
+            Console.WriteLine("Enter ur text"); /// message to user
+            string str = Convert.ToString(Console.ReadLine()); /// string entered by the user
+            char[] s = str.ToCharArray(); /// array 
+            Array.Reverse(s); Array.Reverse(s); /// mirroring method
+            Console.WriteLine(s); /// output to the user
+            Console.ReadKey(); /// exit from the program
+        }
+    }
+}
+
